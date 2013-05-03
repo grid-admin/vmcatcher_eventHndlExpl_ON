921943198109159dff552befcc4c421b31117945
Owen Synge <owen.synge@desy.de>
2013-05-04 00:56:50 +0200

    made more independent

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from vmcatcher.__version__ import version
+#!/usr/bin/env python
 from sys import version_info
 if version_info < (2, 6):
 	from distutils.core import setup
@@ -9,14 +9,16 @@ else:
         	from ez_setup import use_setuptools
         	use_setuptools()
         	from setuptools import setup, find_packages
-            
+
+
+version = "0.0.1"
+
 setup(name='vmcatcher_eventHndlExpl_ON',
     version=version,
     description="vmcatcher_eventHndlExpl_ON manages images in fedcloud",
     author="R Rosende Dopazo",
     author_email="rrosende@cesga.es",
     url="www.cesga.es",
-    
-    data_files=[('/usr/share/doc/vmcatcher-%s' % (version), ['vmcatcher_eventHndlExpl_ON']),
-		('/usr/share/doc/vmcatcher-%s/examples/'% (version), ['vmcatcher-cron.cfg'])]
+    scripts=['vmcatcher_eventHndlExpl_ON'],
+    data_files=[('/usr/share/doc/vmcatcher-%s/examples/'% (version), ['vmcatcher-cron.cfg'])]
 )
