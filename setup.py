b428cdb60f69b6796d71b43662e01dc18063b05d
alvarosimon <varosim@gmail.com>
2014-04-16 17:30:03 +0200

    Increased setup.py version to 0.0.3

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ else:
         	from setuptools import setup, find_packages
 
 
-version = "0.0.2"
+version = "0.0.3"
 
 setup(name='vmcatcher_eventHndlExpl',
     version=version,
     description="vmcatcher_eventHndl_ON manages images in fedcloud",
     author="R Rosende Dopazo",
-    author_email="rrosende@cesga.es",
+    author_email="grid-admin@cesga.es",
     url="www.cesga.es",
     scripts=['vmcatcher_eventHndl_ON'],
     data_files=[('/usr/share/doc/vmcatcher-%s/examples/'% (version), ['vmcatcher-cron.cfg'])]
